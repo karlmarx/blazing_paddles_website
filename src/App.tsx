487b9bb225c5a753089dd0bea13eb50a8e17ef02
Karl Marx-Levi <kmarx-levi@smc3.com>
2023-12-16 10:46:25 -0500

    reverse engineered site from karlmarx.github.io

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./index.css";
+import MaterialNavbar from "./components/MaterialNavbar";
+import { Typography } from "@material-tailwind/react";
+import logo from "./assets/images/logoblazingpaddles.jpg"; // with import
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div
+          className="App"
+          style={{
+            backgroundColor: "black",
+            // width: '100px',
+            height: "100vh",
+          }}
+      >
+        <MaterialNavbar />
+
+        {/*<div className="text-align-center">*/}
+        {/*<div className="flex justify-content-center align-items-center">*/}
+        <div className="hidden md:inline-flex my-10 shadow rounded-sm w-full  justify-center">
+          <div>
+            <img
+                className="h-96 w-96 rounded-full object-cover object-center"
+                src={logo}
+            />
+          </div>
+        </div>
+        <div>
+          <p>
+            <Typography variant="h1" color="yellow" textGradient>
+              Blazing Paddles
+            </Typography>
+          </p>
+        </div>
+        {/*</div>*/}
+        {/*</div>*/}
+        {/*<header className="App-header text-3xl font-bold underline text-red-600">*/}
+        {/*  /!*<img src={logo} className="App-logo" alt="logo" />*!/*/}
+        {/*  <p>blazing paddles</p>*/}
+        {/*</header>*/}
+      </div>
   );
 }
 
---
+++ src/components/MaterialNavbar.tsx
@@ -0,0 +1,397 @@
+import type { NavbarProps } from "@material-tailwind/react";
+
+import React from "react";
+import {
+    Navbar,
+    MobileNav,
+    Typography,
+    Button,
+    Menu,
+    MenuHandler,
+    MenuList,
+    MenuItem,
+    Avatar,
+    Card,
+    IconButton,
+} from "@material-tailwind/react";
+import {
+    CubeTransparentIcon,
+    UserCircleIcon,
+    CodeBracketSquareIcon,
+    Square3Stack3DIcon,
+    ChevronDownIcon,
+    Cog6ToothIcon,
+    InboxArrowDownIcon,
+    LifebuoyIcon,
+    PowerIcon,
+    RocketLaunchIcon,
+    Bars2Icon,
+    PhotoIcon,
+    CalendarDaysIcon,
+    MapPinIcon,
+    GiftIcon,
+    ChatBubbleLeftRightIcon,
+} from "@heroicons/react/24/outline";
+
+// profile menu component
+const profileMenuItems = [
+    {
+        label: "My Profile",
+        icon: UserCircleIcon,
+    },
+    {
+        label: "Edit Profile",
+        icon: Cog6ToothIcon,
+    },
+    {
+        label: "Inbox",
+        icon: InboxArrowDownIcon,
+    },
+    {
+        label: "Help",
+        icon: LifebuoyIcon,
+    },
+    {
+        label: "Sign Out",
+        icon: PowerIcon,
+    },
+];
+
+function ProfileMenu() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const closeMenu = () => setIsMenuOpen(false);
+
+    return (
+        <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+            <MenuHandler>
+                <Button
+                    variant="text"
+                    color="blue-gray"
+                    className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
+                                    >
+                    <Avatar
+                        variant="circular"
+                        size="sm"
+                        alt="tania andrew"
+                        className="border border-gray-900 p-0.5"
+                        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+                    />
+                    <ChevronDownIcon
+                        strokeWidth={2.5}
+                        className={`h-3 w-3 transition-transform ${
+                            isMenuOpen ? "rotate-180" : ""
+                        }`}
+                    />
+                </Button>
+            </MenuHandler>
+            <MenuList className="p-1">
+                {profileMenuItems.map(({ label, icon }, key) => {
+                    const isLastItem = key === profileMenuItems.length - 1;
+                    return (
+                        <MenuItem
+                            key={label}
+                            onClick={closeMenu}
+                            className={`flex items-center gap-2 rounded ${
+                                isLastItem
+                                    ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+                                    : ""
+                            }`}
+                        >
+                            {React.createElement(icon, {
+                                className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
+                                strokeWidth: 2,
+                            })}
+                            <Typography
+                                as="span"
+                                variant="small"
+                                className="font-normal"
+                                color={isLastItem ? "red" : "inherit"}
+                            >
+                                {label}
+                            </Typography>
+                        </MenuItem>
+                    );
+                })}
+            </MenuList>
+        </Menu>
+    );
+}
+
+// nav list menu
+const navListMenuItems = [
+    {
+        title: "Dragonboat",
+        description:
+            "Learn how to use @material-tailwind/react, packed with rich components for React.",
+    },
+    {
+        title: "Outrigger Canoe",
+        description:
+            "A complete set of UI Elements for building faster websites in less time.",
+    },
+    {
+        title: "Canoe",
+        description:
+            "A complete set of UI Elements for building faster websites in less time.",
+    },
+];
+
+function NavListMenu() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const renderItems = navListMenuItems.map(({ title, description }) => (
+        <a href="#" key={title}>
+            <MenuItem>
+                <Typography variant="h6" color="blue-gray" className="mb-1">
+                    {title}
+                </Typography>
+                <Typography variant="small" color="gray" className="font-normal">
+                    {description}
+                </Typography>
+            </MenuItem>
+        </a>
+    ));
+
+    return (
+        <React.Fragment>
+            <Menu allowHover open={isMenuOpen} handler={setIsMenuOpen}>
+                <MenuHandler>
+                    <Typography as="a" href="#" variant="small" className="font-normal">
+                        <MenuItem className="hidden items-center gap-2 text-blue-gray-900 lg:flex lg:rounded-full">
+                            <Square3Stack3DIcon className="h-[18px] w-[18px]" />
+                            Paddling{" "}
+                            <ChevronDownIcon
+                                strokeWidth={2}
+                                className={`h-3 w-3 transition-transform ${
+                                    isMenuOpen ? "rotate-180" : ""
+                                }`}
+                            />
+                        </MenuItem>
+                    </Typography>
+                </MenuHandler>
+                <MenuList className="hidden w-[36rem] grid-cols-7 gap-3 overflow-visible lg:grid"  >
+                    <Card
+                        color="blue"
+                        shadow={false}
+                        variant="gradient"
+                        className="col-span-3 grid h-full w-full place-items-center rounded-md"
+                    >
+                        <RocketLaunchIcon strokeWidth={1} className="h-28 w-28" />
+                    </Card>
+                    <ul className="col-span-4 flex w-full flex-col gap-1">
+                        {renderItems}
+                    </ul>
+                </MenuList>
+            </Menu>
+            <MenuItem className="flex items-center gap-2 text-blue-gray-900 lg:hidden">
+                <Square3Stack3DIcon className="h-[18px] w-[18px]" /> Paddle{" "}
+            </MenuItem>
+
+            <ul className="ml-6 flex w-full flex-col gap-1 lg:hidden">
+                {renderItems}
+            </ul>
+        </React.Fragment>
+    );
+}
+
+const teamNavListMenuItems = [
+    {
+        title: "About Us",
+        description:
+            "Learn how to use @material-tailwind/html, packed with rich components and widgets.",
+    },
+    {
+        title: "Community Service",
+        description:
+            "Learn how to use @material-tailwind/react, packed with rich components for React.",
+    },
+    {
+        title: "Awards and Recognition",
+        description:
+            "A complete set of UI Elements for building faster websites in less time.",
+    },
+    {
+        title: "Canoe",
+        description:
+            "A complete set of UI Elements for building faster websites in less time.",
+    },
+];
+function TeamNavListMenu() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const renderItems = teamNavListMenuItems.map(({ title, description }) => (
+        <a href="#" key={title}>
+            <MenuItem>
+                <Typography variant="h6" color="blue-gray" className="mb-1">
+                    {title}
+                </Typography>
+                <Typography variant="small" color="gray" className="font-normal">
+                    {description}
+                </Typography>
+            </MenuItem>
+        </a>
+    ));
+
+    return (
+        <React.Fragment>
+            <Menu allowHover open={isMenuOpen} handler={setIsMenuOpen}>
+                <MenuHandler>
+                    <Typography as="a" href="#" variant="small" className="font-normal">
+                        <MenuItem className="hidden items-center gap-2 text-blue-gray-900 lg:flex lg:rounded-full">
+                            <Square3Stack3DIcon className="h-[18px] w-[18px]" /> Team{" "}
+                            <ChevronDownIcon
+                                strokeWidth={2}
+                                className={`h-3 w-3 transition-transform ${
+                                    isMenuOpen ? "rotate-180" : ""
+                                }`}
+                            />
+                        </MenuItem>
+                    </Typography>
+                </MenuHandler>
+                <MenuList className="hidden w-[36rem] grid-cols-7 gap-3 overflow-visible lg:grid">
+                    <Card
+                        color="blue"
+                        shadow={false}
+                        variant="gradient"
+                        className="col-span-3 grid h-full w-full place-items-center rounded-md"
+                    >
+                        <RocketLaunchIcon strokeWidth={1} className="h-28 w-28" />
+                    </Card>
+                    <ul className="col-span-4 flex w-full flex-col gap-1">
+                        {renderItems}
+                    </ul>
+                </MenuList>
+            </Menu>
+            <MenuItem className="flex items-center gap-2 text-blue-gray-900 lg:hidden">
+                <Square3Stack3DIcon className="h-[18px] w-[18px]" /> Team{" "}
+            </MenuItem>
+
+            <ul className="ml-6 flex w-full flex-col gap-1 lg:hidden">
+                {renderItems}
+            </ul>
+        </React.Fragment>
+    );
+}
+
+// nav list component
+const navListItems = [
+    {
+        label: "Gallery",
+        icon: PhotoIcon,
+    },
+    {
+        label: "Location",
+        icon: MapPinIcon,
+    },
+    {
+        label: "Events",
+        icon: CalendarDaysIcon,
+    },
+    {
+        label: "Donate",
+        icon: GiftIcon,
+    },
+    {
+        label: "Contact",
+        icon: ChatBubbleLeftRightIcon,
+    },
+];
+
+function NavList() {
+    return (
+        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center">
+            <NavListMenu />
+            <TeamNavListMenu />
+            {navListItems.map(({ label, icon }, key) => (
+                <Typography
+                    key={label}
+                    as="a"
+                    href="#"
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal"
+                >
+                    <MenuItem className="flex items-center gap-2 lg:rounded-full">
+                        {React.createElement(icon, { className: "h-[18px] w-[18px]" })}{" "}
+                        {label}
+                    </MenuItem>
+                </Typography>
+            ))}
+            {/*<MenuItem className="relative flex w-full gap-2 md:w-max">*/}
+            {/*  <Button*/}
+            {/*    variant="gradient"*/}
+            {/*    size="sm"*/}
+            {/*    fullWidth*/}
+            {/*    className="flex items-center gap-3 lg:ml-auto"*/}
+            {/*    // flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto*/}
+            {/*  >*/}
+            {/*    <span>Try Dragonboat</span>*/}
+            {/*  </Button>*/}
+            {/*</MenuItem>*/}
+        </ul>
+    );
+}
+
+export default function MaterialNavbar() {
+    const [isNavOpen, setIsNavOpen] = React.useState(false);
+
+    const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
+
+    React.useEffect(() => {
+        window.addEventListener(
+            "resize",
+            () => window.innerWidth >= 960 && setIsNavOpen(false),
+        );
+    }, []);
+
+    return (
+        // <Navbar className="mx-auto max-w-screen-xl p-2 lg:rounded-full lg:pl-6">
+        <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4">
+            <div className="relative mx-auto flex flex-wrap justify-between items-center text-blue-gray-900">
+                {/*<div className="flex flex-wrap items-center justify-between gap-y-4 ">*/}
+                <Typography
+                    as="a"
+                    href="#"
+                    className="mr-4 ml-2 cursor-pointer py-1.5 font-medium"
+                >
+                    Blazing Paddles
+                </Typography>
+                <div className="absolute top-2/4 left-2/4 hidden -translate-x-2/4 -translate-y-2/4 lg:block">
+                    {/*<div className="flex items-center gap-4">*/}
+                    {/*<div className="hidden lg:block">*/}
+                    <NavList />
+                </div>
+
+                {/*<div className="relative flex w-full gap-2 md:w-max">*/}
+                <Button
+                    variant="gradient"
+                    size="sm"
+                    fullWidth
+                    className="items-center flex w-full gap-2 md:w-max"
+                    // flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="w-5 h-5"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M13.5 4.938a7 7 0 11-9.006 1.737c.202-.257.59-.218.793.039.278.352.594.672.943.954.332.269.786-.049.773-.476a5.977 5.977 0 01.572-2.759 6.026 6.026 0 012.486-2.665c.247-.14.55-.016.677.238A6.967 6.967 0 0013.5 4.938zM14 12a4 4 0 01-4 4c-1.913 0-3.52-1.398-3.91-3.182-.093-.429.44-.643.814-.413a4.043 4.043 0 001.601.564c.303.038.531-.24.51-.544a5.975 5.975 0 011.315-4.192.447.447 0 01.431-.16A4.001 4.001 0 0114 12z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+
+                    <span>Try Dragonboat</span>
+                </Button>
+                {/*</div>*/}
+            </div>
+
+            <MobileNav open={isNavOpen} className="overflow-scroll">
+                <NavList />
+            </MobileNav>
+        </Navbar>
+    );
+}
